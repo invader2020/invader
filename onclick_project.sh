69d97dd9b683e4890f3e43f59c93c61b84d148a6
invader2020 <59687267+invader2020@users.noreply.github.com>
2020-01-20 07:15:06 +0000

    Add files via upload

---
+++ onclick_project.sh
@@ -0,0 +1,293 @@
+echo "       © ᴘᴇɴᴇᴛʀᴀᴛᴏʀs                  ᴄᴏʀᴘᴏʀᴀᴛɪᴏɴ ©      "
+echo "          ♛ α cσмρℓεтεℓү αυтσмαтε∂ ρяσנεcт ♛         "
+echo "                ♛ Creator : Sarkaar    ♛            "
+echo "                ♛ Support : Xanderdark ♛            "
+echo "           ______             _                 "
+echo "          / _____)           | |                "
+echo "         ( (____  _____  ____| |  _ _____  ____ "
+echo "          \____ \(____ |/ ___| |_/ (____ |/ ___)"
+echo "          _____) / ___ | |   |  _ (/ ___ | |    "
+echo "         (______/\_____|_|   |_| \_\_____|_|  "
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉ 46%"
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " "
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉ 88%"
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " "
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉ 100%"
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " "
+echo "𝕮𝖔𝖒𝖕𝖑𝖊𝖙𝖊𝖉"
+echo " "
+cp -R /storage/emulated/0/Download/MODPUBGKR/com.pubg.krmobile/ /storage/emulated/0/Android/data/com.pubg.krmobile/
+
+COUNT=50
+fixPUBGM()
+{
+cp /sdcard/Android/data/com.PUBGKR/* /data/data/com.pubg.krmobile/lib
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs__db_issues
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs__db_key_values
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs__db_properties
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs_db_helpshift_users
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs__db_support_key_values
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs__db_support_key_values
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs_db_helpshift_users
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs__db_sessions
+chmod 660 /data/data/com.pubg.krmobile/databases/__hs_log_store
+chmod 660 /data/data/com.pubg.krmobile/databases/bugly_db_
+chmod 660 /data/data/com.pubg.krmobile/databases/config.db
+chmod 660 /data/data/com.pubg.krmobile/databases/iMSDK.db
+chmod 755 /data/data/com.pubg.krmobile/lib/*
+chmod 000 /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/1950038955_3090_0.16.5.11560_20200108020124_1155095217_cures.ifs.res
+chmod 000 /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.0.11466.pak
+sleep 10
+}
+modPUBGM()
+{
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs__db_issues
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs__db_key_values
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs__db_properties
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs_db_helpshift_users
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs__db_support_key_values
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs_db_helpshift_users
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs__db_sessions
+chmod 000 /data/data/com.pubg.krmobile/databases/__hs_log_store
+chmod 000 /data/data/com.pubg.krmobile/databases/bugly_db_
+chmod 000 /data/data/com.pubg.krmobile/databases/config.db
+chmod 000 /data/data/com.pubg.krmobile/databases/iMSDK.db
+rm -rf /data/data/com.pubg.krmobile/lib/libgcloud.so
+rm -rf /data/data/com.pubg.krmobile/lib/libBugly.so
+rm -rf /data/data/com.pubg.krmobile/lib/libtprt.so
+rm -rf /data/data/com.pubg.krmobile/lib/libUE4.so
+chmod 000 /data/data/com.pubg.krmobile/lib/libzip.so
+chmod 000 /data/data/com.pubg.krmobile/lib/libzlib.so
+sleep 10
+}
+cleaner()
+{
+rm -rf /data/data/com.pubg.krmobile/app_appcache
+rm -rf /data/data/com.pubg.krmobile/app_bugly
+rm -rf /data/data/com.pubg.krmobile/cache
+rm -rf /data/data/com.pubg.krmobile/code_cache
+rm -rf /data/data/com.pubg.krmobile/app_crashrecord
+rm -rf /data/data/com.pubg.krmobile/databases/google_app_measurement_local.db-journal
+rm -rf /data/data/com.pubg.krmobile/databases/pri_tencent_analysis.db-journal
+rm -rf /data/data/com.pubg.krmobile/databases/tencent_analysis.db-journal
+rm -rf /data/data/com.pubg.krmobile/databases/xg_message.db-journal
+rm -rf /data/data/com.pubg.krmobile/databases/google_app_measurement_local.db
+rm -rf /data/data/com.pubg.krmobile/databases/pri_tencent_analysis.db
+rm -rf /data/data/com.pubg.krmobile/databases/__hs__db_issues-journal
+rm -rf /data/data/com.pubg.krmobile/databases/__hs__db_key_values-journal
+rm -rf /data/data/com.pubg.krmobile/databases/__hs__db_sessions-journal
+rm -rf /data/data/com.pubg.krmobile/databases/__hs__db_support_key_values-journal
+rm -rf /data/data/com.pubg.krmobile/databases/bugly_db_-journal
+rm -rf /data/data/com.pubg.krmobile/databases/__hs_log_store-journal
+rm -rf /data/data/com.pubg.krmobile/files/iMSDK
+rm -rf /data/data/com.pubg.krmobile/files/tss_tmp
+rm -rf /data/data/com.pubg.krmobile/files/ss_tmp
+rm -rf /data/data/com.pubg.krmobile/files/AppEventsLogger.persistedevents
+rm -rf /data/data/com.pubg.krmobile/files/tpnlcache.data
+rm -rf /sdcard/tencent
+rm -rf /sdcard/MidasOversea
+rm -rf /sdcard/Android/data/com.pubg.krmobile/cache
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/tbslog
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo/RoleInfo.json
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Screenshots
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/vmpcloudconfig.json
+chmod 000 /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/1950038955_3090_0.16.5.11560_20200108020124_1155095217_cures.ifs.res
+chmod 000 /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.0.11466.pak
+printf "Clearing Automatically\n";
+printf "Now: ";
+date
+printf "\n";
+}
+echo ""
+echo "       © ᴘᴇɴᴇᴛʀᴀᴛᴏʀs                  ᴄᴏʀᴘᴏʀᴀᴛɪᴏɴ ©      "
+echo "          ♛ α cσмρℓεтεℓү αυтσмαтε∂ ρяσנεcт ♛         "
+echo "                ♛ Creator : Sarkaar    ♛            "
+echo "                ♛ Support : Xanderdark ♛            "
+echo "           ______             _                 "
+echo "          / _____)           | |                "
+echo "         ( (____  _____  ____| |  _ _____  ____ "
+echo "          \____ \(____ |/ ___| |_/ (____ |/ ___)"
+echo "          _____) / ___ | |   |  _ (/ ___ | |    "
+echo "         (______/\_____|_|   |_| \_\_____|_|  "
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo ""
+echo "𝚁𝙴𝙰𝙳𝚈 𝚃𝙾 𝙿𝙰𝚁𝚃𝚈  ̿’ ̿’\̵͇̿̿\з=(•̪●)=ε/̵͇̿̿/’̿’̿  ̿  ̿ "
+echo ""
+
+if [ -d "/data/data/com.pubg.krmobile" ];
+then
+echo "\nLoading...\n"
+if [ ! -d "/sdcard/Android/data/com.PUBGKR" ];
+then
+if [ ! -f "/data/data/com.pubg.krmobile/lib/libUE4.so" ];
+then
+echo "like PUBGM is not installed Correctly!"
+exit 0;
+fi
+if [ ! -d "/sdcard/Android/data/com.PUBGKR" ];
+then
+mkdir /sdcard/Android/data/com.PUBGKR
+fi
+mkdir /sdcard/Android/data/com.PUBGKR
+cp -R /data/data/com.pubg.krmobile/lib/* /sdcard/Android/data/com.PUBGKR
+echo ""
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "             ♛ Payload Injected ✔️"
+echo "             ♛ Fuck The Server ✔️"
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo ""
+sleep 10
+fi
+else
+printf "\n𝙂𝘼𝙈𝙀 𝙑𝙀𝙍𝙎𝙄𝙊𝙉 𝙉𝙊𝙏 𝙄𝙉𝙎𝙏𝘼𝙇𝙀𝘿";
+exit 0;
+fi
+if [ -d "/data/data/com.cgggxabhi" ];
+then
+echo "Starting Cheat Gamer Game Guardian...\n"
+sleep 2
+am start -n com.cgggxabhi/com.cgggxabhi.MainActivity
+sleep 2
+fi
+if [ -d "/data/data/any_.body_.can_.fuck_.tencent_" ];
+then
+echo "Starting Prabal Gaming Game Guardian...\n"
+sleep 2
+am start -n any_.body_.can_.fuck_.tencent_/any_.body_.can_.fuck_.tencent_.MainActivity
+sleep 2
+fi
+if [ -d "/data/data/com.rjvsbmhdspmnfbame" ];
+then
+echo "Starting TCA Mod Game Guardian...\n"
+sleep 2
+am start -n com.rjvsbmhdspmnfbame/com.rjvsbmhdspmnfbame.MainActivity
+sleep 2
+fi
+if [ -d "/data/data/com.s.fyojrme" ];
+then
+echo "Starting XPLAY Game Guardian...\n"
+sleep 2
+am start -n com.s.fyojrme/com.s.fyojrme.MainActivity
+sleep 2
+fi
+MTPKG='bin.mt.plus'
+if [ $(pidof $MTPKG) ];
+then
+am start -n bin.mt.plus/bin.mt.plus.Main
+fi
+sleep 2
+echo "𝙁𝙐𝘾𝙆𝙄𝙉𝙂 𝙏𝙀𝙉𝘾𝙀𝙉𝙏\n"
+sleep 2
+echo "𝕽𝖊𝖕𝖆𝖎𝖗𝖊𝖉 𝕲𝖆𝖒𝖊𝖘...\n"
+fixPUBGM
+printf "\n➩𝕾𝖙𝖆𝖗𝖙𝖎𝖓𝖌 𝕲𝖆𝖒𝖊𝖘\n";
+sleep 3
+am start -n com.pubg.krmobile/com.epicgames.ue4.SplashActivity
+sleep 7
+modPUBGM
+echo "\n𝔴𝔞𝔦𝔱 𝔣𝔬𝔯 40𝔰𝔢𝔠𝔬𝔫𝔡𝔰"
+sleep 40
+echo "𝔖𝔢𝔱𝔱𝔦𝔫𝔤𝔰 𝔲𝔭 𝔉𝔦𝔩𝔢𝔰 & 𝔓𝔢𝔯𝔪𝔦𝔰𝔰𝔦𝔬𝔫"
+sleep 10
+echo "\n➩ℭ𝔩𝔢𝔞𝔫𝔦𝔫𝔤 𝔏𝔬𝔤𝔰..."
+printf "\n\n";
+PACKAGE='com.pubg.krmobile'
+while [ $(pidof $PACKAGE) ]
+do
+cleaner
+((COUNT=$COUNT+10))
+if [ ! $(pidof $PACKAGE) ]; then
+break
+fi
+sleep 10
+done
+am force-stop com.cgggxabhi
+am force-stop any_.body_.can_.fuck_.tencent_
+am force-stop com.rjvsbmhdspmnfbame
+am force-stop com.s.fyojrme
+sleep 3
+MTMPKG='bin.mt.plus'
+if [ $(pidof $MTMPKG) ];
+then
+am start -n bin.mt.plus/bin.mt.plus.Main
+fi
+echo ""
+echo ""
+((final=$COUNT/60))
+printf "➩ 𝚈𝚘𝚞 𝚙𝚕𝚊𝚢𝚒𝚗𝚐 𝙿𝚄𝙱𝙶 𝙼𝚘𝚋𝚒𝚕𝚎  ";
+echo $final
+printf "Minutes \n";
+printf "\n\n";
+sleep 3
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "sᴇᴇᴍs ᴘᴜʙɢ ᴄʟᴏsᴇᴅ"
+echo "ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ᴅᴀʀᴋ"
+date
+echo "ʀᴇsᴛᴏʀɪɴɢ ᴄʟɪɴᴛ"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "𒊹 ����🅽��🆃������🅾��🆂 ��"
+echo "     "
+echo "       Flush-engine━=͟͟͞͞⚈͒⚈͒•"
+echo "             ●█〓██▄▄▄▄▄▄●●●SARKAR●●● "
+echo  "       ▄▅██████▅▄▃▂ "
+echo "       ██████████████ "
+echo "       ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲◤ "
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+fixPUBGM
+busybox mount -o rw,remount /system
+busybox rm -rf /system/etc/hosts
+echo "
+127.0.0.1 localhost
+::1 localhost
+" > /system/etc/hosts
+printf "\n\n";
+echo "𝕮𝖔𝖒𝖕𝖑𝖊𝖙𝖊𝖉"
+echo "           ______             _                 "
+echo "          / _____)           | |                "
+echo "         ( (____  _____  ____| |  _ _____  ____ "
+echo "          \____ \(____ |/ ___| |_/ (____ |/ ___)"
+echo "          _____) / ___ | |   |  _ (/ ___ | |    "
+echo "         (______/\_____|_|   |_| \_\_____|_|  "
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "   © ᴘᴇɴᴇᴛʀᴀᴛᴏʀs                         ᴄᴏʀᴘᴏʀᴀᴛɪᴏɴ ©      "
+echo "      ✰||| keep using || ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ |||✰    "
+echo " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " ᴘᴇɴᴇᴛʀᴀᴛᴏʀs                  ᴄᴏʀᴘᴏʀᴀᴛɪᴏɴ  "
+echo " p::::ppp:::::::::p         cc:::::::::::::::c"
+echo " p:::::::::::::::::p       c:::::::::::::::::c"
+echo " pp::::::ppppp::::::p     c:::::::cccccc:::::c"
+echo " p:::::p     p:::::p     c::::::c     ccccccc"
+echo " p:::::p     p:::::p     c:::::c             "
+echo " p:::::p     p:::::p     c:::::c             "
+echo " p:::::p    p::::::p     c::::::c     ccccccc"
+echo " p:::::ppppp:::::::p     c:::::::cccccc:::::c"
+echo " p::::::::::::::::p       c:::::::::::::::::c"
+echo " p::::::::::::::pp         cc:::::::::::::::c"
+echo " p::::::pppppppp            cccccccccccccccc"
+echo " p:::::p                                     "
+echo " p:::::p                                     "
+echo " p:::::::p                                    "
+echo " p:::::::p                                    "
+echo " p:::::::p                                    "
+echo " ppppppppp                                    "
+echo "                "
